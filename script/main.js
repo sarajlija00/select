2e5b91431f22a11f2272fd994942bac0016a8a6c
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-05-31 16:40:15 +0200

    commit poslije sranja koje se desilo prilikom spajanja datoteka

--- script/main.js
+++ script/main.js
@@ -1,52 +1,22 @@
 const errorDiv = document.getElementById('error');
 const token =  `6205c51f2f31438ab76389b3644da184`;
 
-<<<<<<< HEAD
 const getLeagues = () => new Promise ((resolve, reject) => {
     let url = `http://api.football-data.org/v2/competitions`;
-=======
-const getTeams = (leagueCode) => new Promise ((resolve, reject) => {
-    let url = `http://api.football-data.org/v2/competitions/${leagueCode}/teams`;
->>>>>>> 3174c7fd4b5cf8dc5a13d2820971a936a99dbd7b
     let xhttp = new XMLHttpRequest ();
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
             let object = JSON.parse(xhttp.responseText);
             resolve(object);
         }
-<<<<<<< HEAD
         if (xhttp.readyState == 4 && xhttp.status !== 200){
         reject('error loading');
-=======
-        if (xhttp.readyState == 4 && xhttp.status != 200) {
-            reject('error loading teams');
-
         }
     }
     xhttp.open ('GET', url, true);
     xhttp.setRequestHeader('X-Auth-Token',token);
     xhttp.send();
 });
-
-const getLeagues = () => new Promise ((resolve, reject) => {
-    let url = `http://api.football-data.org/v2/competitions/`;
-    let xhttp = new XMLHttpRequest ();
-    xhttp.onreadystatechange = function () {
-        if (xhttp.readyState == 4 && xhttp.status == 200) {
-            let object = JSON.parse(xhttp.responseText);
-            resolve(object);
-        }
-        if (xhttp.readyState == 4 && xhttp.status != 200) {
-
-            reject('error loading');
->>>>>>> 3174c7fd4b5cf8dc5a13d2820971a936a99dbd7b
-        }
-    }
-    xhttp.open ('GET', url, true);
-    xhttp.setRequestHeader('X-Auth-Token',token);
-    xhttp.send();
-});
-<<<<<<< HEAD
     const arrayThroughInTheLeague = () => {
         let options = ``;
         leagues.competitions.forEach(league => {
@@ -65,45 +35,4 @@ const worldLeagueInTheInput = () => {
     getLeagues()
     .then (arrayThroughInTheLeague)
     .catch (error)
-=======
-
-const populateLeagues = () => {
-    let options = ``;
-    getLeagues()
-      .then(leagues => {
-        leagues.competitions.forEach(league => {
-            if(league.code) {
-                options += `
-                    <option value="${league.code}">${league.name}</option>
-                `;
-            }
-        });
-        const leaguesSelect = document.getElementById('leagues');
-        leaguesSelect.innerHTML = options;
-      })
-      .catch(err => console.log('error', err));
-
-}
-
-
-const getLeague = () => {
-    //procitaj value iz league select i pozovi funkciju getTeams
-    // value iz league selecta predstavlja code lige
-    const leaguesSelect = document.getElementById('leagues');
-    let leagueCode = leaguesSelect.options[leaguesSelect.selectedIndex].value;
-
-    let options = '';
-    getTeams(leagueCode)
-      .then(result => {
-            result.teams.forEach(team => {
-                options += `
-                    <option value="${team.id}">${team.name}</option>
-                `;
-            });
-        //   populate teams select
-        const teamsSelect = document.getElementById('teams');
-        teamsSelect.innerHTML = options;
-      })
-      .catch(err => console.log('show error', err))
->>>>>>> 3174c7fd4b5cf8dc5a13d2820971a936a99dbd7b
 }

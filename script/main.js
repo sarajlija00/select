32c54ce99e7ea988964183f3349dfcb9a57081b2
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-05-31 16:03:56 +0200

    corrected

--- script/main.js
+++ script/main.js
@@ -1,8 +1,8 @@
 const errorDiv = document.getElementById('error');
-const token =  `6205c51f2f31438ab76389b3644da184`
+const token =  `6205c51f2f31438ab76389b3644da184`;
 
-const getTeams = () => new Promise ((resolve, reject) => {
-    let url = `http://api.football-data.org/v2/competitions/PD/teams`;
+const getLeagues = () => new Promise ((resolve, reject) => {
+    let url = `http://api.football-data.org/v2/competitions`;
     let xhttp = new XMLHttpRequest ();
     xhttp.onreadystatechange = function () {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -10,9 +10,30 @@ const getTeams = () => new Promise ((resolve, reject) => {
             console.log(object);
             resolve(object);
         }
-        reject(errorDiv);
+        if (xhttp.readyState == 4 && xhttp.status !== 200){
+        reject('error loading');
+        }
     }
     xhttp.open ('GET', url, true);
     xhttp.setRequestHeader('X-Auth-Token',token);
     xhttp.send();
 });
+    const arrayThroughInTheLeague = () => {
+        let options = ``;
+        leagues.competitions.forEach(league => {
+            if (league.code) {
+                options += `
+                <option value="${league.code}">${league.name}</option>
+                `;
+    }
+    const leagueInSelect = document.getElementById('league');
+    leagueInSelect.innerHTML = options;
+});
+
+const error = err => errorDiv.innerHTML = err;
+
+const worldLeagueInTheInput = () => {
+    getLeagues()
+    .then (arrayThroughInTheLeague)
+    .catch (error)
+}
